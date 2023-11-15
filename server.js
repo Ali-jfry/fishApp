9481755e1ffbd84e01c256932ea3088c3f1fb4ac
ali-jfry <alijfry029@gmail.com>
2023-11-15 16:52:16 +1100

    file setups

---
+++ server.js
@@ -0,0 +1,19 @@
+// server.js
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Example route
+app.get('/api/message', (req, res) => {
+    res.json({ message: 'Hello from the backend!' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
